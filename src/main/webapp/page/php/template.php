aacb7641ddb29ab829dab241bd3f974c207179c8
Manish Baxi <baxi.manish@welue.com>
2015-07-24 15:57:24 +0530

    Added PHP templating engine.

--- src/main/webapp/page/php/template.php
+++ src/main/webapp/page/php/template.php
@@ -60,7 +60,7 @@
   <div class="col-md-12">
     <div class="panel panel-info">
       <div class="panel-heading">
-        <h4>JSON representation</h4>
+        <h4>PHP templating engine</h4>
       </div>
       <div class="panel-body">
         <p>
